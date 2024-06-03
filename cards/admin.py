ad2bcbe6b274cd0cb52d974d3554c7bf35783589
lasha_shala <nikolaishvililashan1@gmail.com>
2024-06-03 18:41:01 +0400

    registerd card into admin

--- cards/admin.py
+++ cards/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from cards.models import Card
 
-# Register your models here.
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'censored_number', 'is_valid', 'created_at')
+    list_filter = ('is_valid', 'created_at')
+    search_fields = ('title', 'censored_number')
+    list_per_page = 20
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'title', 'censored_number', 'is_valid')
+        }),
+        (_('Date Information'), {
+            'fields': ('created_at',)
+        }),
+    )
+    readonly_fields = ('created_at',)
+    ordering = ('-created_at',)
